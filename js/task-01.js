fff7d3fa9ab7c4050794cdeb07687916f62e96dc
bartoszkucharuk <95539507+bartoszkucharuk@users.noreply.github.com>
2022-07-28 22:23:32 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,16 @@
 //1
-
-const body = document.body;
-const list = body.querySelector("#categories");
+const list = document.querySelector("#categories");
 const categoriesList = list.querySelectorAll(".item")
 console.log(`Number of categories: ${categoriesList.length}`);
+
+
+//2
+categoriesList.forEach(element => { 
+
+    const category = element.querySelector("h2");
+    console.log(`Category: ${category.textContent}`)
+
+    const quantity = element.querySelectorAll('li');
+        console.log(`Elements: ${quantity.length}`)
+}
+)
