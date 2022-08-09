8b1748955ddd0eb12d22fb30c7b7e2c83e3d8bb4
bartoszkucharuk <95539507+bartoszkucharuk@users.noreply.github.com>
2022-08-09 17:12:52 +0200

    finished 8 task
    starting task 9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit",(event) => {
+event.preventDefault();
+
+    const {
+        elements: { password, email }
+    } = event.currentTarget;
+    
+if (password.value == "" || email.value == ""){
+    return alert("All fields need to be filled before sending");
+}
+
+    const elements = { password: password.value, email: email.value };
+    
+console.log(elements);
+event.currentTarget.reset();
+
+});
