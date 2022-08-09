6ed01ac643fb612daa7c01c0c4290e9c7e2f7eda
bartoszkucharuk <95539507+bartoszkucharuk@users.noreply.github.com>
2022-08-09 17:49:57 +0200

    finishing task 9
    .

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyColor = document.querySelector("body");
+const setColor = document.querySelector(".color");
+const clickButton = document.querySelector(".change-color");
+
+clickButton.addEventListener("click", () => {
+  bodyColor.style.backgroundColor = getRandomHexColor();
+  setColor.textContent = getRandomHexColor();
+});
